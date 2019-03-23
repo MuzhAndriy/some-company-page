89a8dfeaf0076e898b09373331f33f64f9478632
muzhandriy <45791204+muzhandriy@users.noreply.github.com>
2019-03-23 19:17:06 +0200

    added base file structure

---
+++ mode/webpack.dev.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+module.exports = merge(common, {
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, '../docs'),
+        filename: './js/[name].js',
+        publicPath: '/'
+    },
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+        compress: true,
+        port: 9000,
+        contentBase: './src',
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader", options: {
+                        sourceMap: true
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.EvalSourceMapDevToolPlugin({
+            filename: '[file].map'
+        })
+    ]
+});
